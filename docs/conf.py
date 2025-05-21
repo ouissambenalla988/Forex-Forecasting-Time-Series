e7f4bacd225e9b6e4f37bc5b161bd40d78dae0dc
Ouissam Benalla <ouissambenalla.ia@gmail.com>
2025-05-21 10:40:31 +0100

    Add documentation for Read the Docs

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import os
+import sys
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Forex-Forecasting-Time-Series'
+copyright = '2025, BENALLA Ouissam'
+author = 'BENALLA Ouissam'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+Bienvenue dans la documentation Forex-Forecasting-Time-Series
+=============================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contenu
+
+   modules
+
+Indices et tables
+=================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
